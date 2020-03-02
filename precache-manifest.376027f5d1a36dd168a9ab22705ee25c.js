e6c31d781242c3ecd3a3ff2c23aabcbc89dad5fe
Cristian Pavel <chiarpavel@gmail.com>
2020-03-02 17:14:49 +0200

    add build 2

---
+++ precache-manifest.376027f5d1a36dd168a9ab22705ee25c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8108d492b6a8c0dbcc805325114000",
+    "url": "/devtest/index.html"
+  },
+  {
+    "revision": "9f4ab835b5c89c944cd0",
+    "url": "/devtest/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "19c08a860fe8ce7f5040",
+    "url": "/devtest/static/css/main.7f0ce80f.chunk.css"
+  },
+  {
+    "revision": "9f4ab835b5c89c944cd0",
+    "url": "/devtest/static/js/2.8ff2e4dc.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/devtest/static/js/2.8ff2e4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c08a860fe8ce7f5040",
+    "url": "/devtest/static/js/main.fb70ecd5.chunk.js"
+  },
+  {
+    "revision": "2b1a2ef3918608fc3ec7",
+    "url": "/devtest/static/js/runtime-main.a408e852.js"
+  }
+]);
